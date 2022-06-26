903b82fc9981d329ca79870c6a09eda75913d25f
Dodamking <power6120@naver.com>
2022-06-27 00:39:45 +0900

    로그아웃 오류 해결

--- routes/auth.js
+++ routes/auth.js
@@ -53,7 +53,7 @@ router.post('/login', isNotLoggedIn, (req, res, next) => {
 });
 
 router.get('/logout', isLoggedIn, (req, res) => {
-    req.logout();
+    // req.logout();
     req.session.destroy();
     res.redirect('/');
 });
