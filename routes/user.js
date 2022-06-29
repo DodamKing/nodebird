f67c743b04e68cc6495a9c92ef1c311aa0c07cc4
Dodamking <power6120@naver.com>
2022-06-30 00:48:34 +0900

    언팔성공

--- routes/user.js
+++ routes/user.js
@@ -15,7 +15,31 @@ router.post('/:id/follow', isLoggedIn, async (req, res, next) => {
 });
 
 router.post('/:id/unfol', isLoggedIn, async (req, res, next) => {
-    
+    try {
+        console.log(req.params.id);
+        console.log(req.query);
+        // const user = await User.findOne({where : {id : req.params.id}});
+        // console.log(user);
+    } catch {
+        console.error(error);
+        next(error);
+    }
+});
+
+router.post('/unfol', isLoggedIn, async (req, res, next) => {
+    try {
+        const idx = req.session.passport.user;
+        const userIdx = req.query.id;
+
+        const user = await User.findOne({where : {id : userIdx}});
+        console.log(userIdx, idx);
+
+        await user.removeFollower(idx);
+        res.send(200);
+    } catch {
+        console.error(error);
+        next(error);
+    }
 });
 
 module.exports = router;
